83d2a045b2104d7fceb3c99fdc3fc68d692676b9
gustavo-srodrigues <gustavogxts@outlook.com>
2024-12-30 21:05:59 -0300

    class

--- __main__.py
+++ __main__.py
@@ -1,2 +1,9 @@
 class Livro:
-    
+    def __init__(self, titulo, ano):
+        self.titulo = titulo
+        self.ano = ano
+
+
+livro1 = Livro("o nome do vento", 2009)
+
+print(f'{livro1.titulo} e data de publicação: {livro1.ano}')
